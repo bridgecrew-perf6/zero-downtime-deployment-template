c6b2a0079f3d8462361af464c8bdf72132718e93
yoon <dudwls143@gmail.com>
2022-05-22 02:42:37 +0900

    test

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -1,29 +1,25 @@
-# switch.sh
-
 #!/bin/bash
 
-# Crawl current connected port of WAS
-CURRENT_PORT=$(cat /home/ec2-user/service_url.inc  | grep -Po '[0-9]+' | tail -1)
+CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
-echo "> Nginx currently proxies to ${CURRENT_PORT}."
+echo "> Current port of running WAS is ${CURRENT_PORT}."
 
-# Toggle port number
 if [ ${CURRENT_PORT} -eq 8081 ]; then
-    TARGET_PORT=8082
+  TARGET_PORT=8082
 elif [ ${CURRENT_PORT} -eq 8082 ]; then
-    TARGET_PORT=8081
+  TARGET_PORT=8081
 else
-    echo "> No WAS is connected to nginx"
-    exit 1
+  echo "> No WAS is connected to nginx"
 fi
 
-# Change proxying port into target port
-echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ec2-user/service_url.inc
+TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
 
-echo "> Now Nginx proxies to ${TARGET_PORT}."
+if [ ! -z ${TARGET_PID} ]; then
+  echo "> Kill WAS running at ${TARGET_PORT}."
+  sudo kill ${TARGET_PID}
+fi
 
-# Reload nginx
-sudo service nginx reload
-
-echo "> Nginx reloaded."
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/playground-logging/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
+echo "> Now new WAS runs at ${TARGET_PORT}."
+exit 0
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,26 +1,29 @@
+# switch.sh
 
 #!/bin/bash
 
-CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
+# Crawl current connected port of WAS
+CURRENT_PORT=$(cat /home/ec2-user/service_url.inc  | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
-echo "> Current port of running WAS is ${CURRENT_PORT}."
+echo "> Nginx currently proxies to ${CURRENT_PORT}."
 
+# Toggle port number
 if [ ${CURRENT_PORT} -eq 8081 ]; then
-  TARGET_PORT=8082
+    TARGET_PORT=8082
 elif [ ${CURRENT_PORT} -eq 8082 ]; then
-  TARGET_PORT=8081
+    TARGET_PORT=8081
 else
-  echo "> No WAS is connected to nginx"
+    echo "> No WAS is connected to nginx"
+    exit 1
 fi
 
-TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+# Change proxying port into target port
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ec2-user/service_url.inc
 
-if [ ! -z ${TARGET_PID} ]; then
-  echo "> Kill WAS running at ${TARGET_PORT}."
-  sudo kill ${TARGET_PID}
-fi
+echo "> Now Nginx proxies to ${TARGET_PORT}."
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/playground-logging/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
-echo "> Now new WAS runs at ${TARGET_PORT}."
-exit 0
+# Reload nginx
+sudo service nginx reload
+
+echo "> Nginx reloaded."
