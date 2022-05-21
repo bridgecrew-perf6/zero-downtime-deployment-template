ee279807d1dab41145b3cb84361a62edba547e81
yoon <dudwls143@gmail.com>
2022-05-21 22:49:15 +0900

    init

---
+++ src/test/java/dev/yoon/nginxtest/NginxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yoon.nginxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginxTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
